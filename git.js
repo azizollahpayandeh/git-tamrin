71f12848e57ab408542262d954d790378a38bc81
azizollah <azizollahpayandeh1@gmail.com>
2023-08-25 12:39:28 +0430

    updateRe

---
+++ git.js
@@ -0,0 +1,38 @@
+const number = 26;
+const btn = document.getElementById('btn')
+
+if (number > 25) {
+    console.log('bigger');
+}else{
+    console.log('smaller than 25');
+}
+
+
+function btnFunc () {
+    alert('click shod')
+}
+
+btn.addEventListener('click', btnFunc)
+
+
+function update () {
+    return true
+}
+
+function authpdate() {
+    return true
+}
+function update () {
+    return true
+}
+
+function authpdate() {
+    return true
+}
+function update () {
+    return true
+}
+
+function authpdate() {
+    return true
+}
